091d2e8706d7d3a97857ec0b9b08baa0a548b1f4
NikitaT1 <nik.tati1@gmail.com>
2019-09-06 12:56:38 +0300

    add map

--- src/TodoListTasks.js
+++ src/TodoListTasks.js
@@ -5,13 +5,12 @@ import TodoListTask from "./TodoListTask";
 class TodoListTasks extends React.Component {
     render = () => {
 
+        let taskElements = this.props.tasks.map (task => <TodoListTask title={task.title} isDone={task.isDone}/>)
+
         return (
 
                     <div className="todoList-tasks">
-                        <TodoListTask title={this.props.tasks[0].title} isDone={this.props.tasks[0].isDone}/>
-                        <TodoListTask title={this.props.tasks[1].title} isDone={this.props.tasks[1].isDone}/>
-                        <TodoListTask title={this.props.tasks[2].title} isDone={this.props.tasks[2].isDone}/>
-                        <TodoListTask title={this.props.tasks[3].title} isDone={this.props.tasks[3].isDone}/>
+                        {taskElements}
                     </div>
 
         );
