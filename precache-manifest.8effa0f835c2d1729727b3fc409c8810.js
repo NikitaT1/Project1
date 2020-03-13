b460eb9be72cbc1b058db7874b0ce5cccc5f7ad8
NikitaT1 <nik.tati1@gmail.com>
2020-03-13 10:57:09 +0300

    Updates

---
+++ precache-manifest.8effa0f835c2d1729727b3fc409c8810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0449437392a2a394cc1ed27fb090ba83",
+    "url": "/Project1/index.html"
+  },
+  {
+    "revision": "b5a1abc2693da821edac",
+    "url": "/Project1/static/css/main.bf322cc8.chunk.css"
+  },
+  {
+    "revision": "1b4820ac64d95cb0e35e",
+    "url": "/Project1/static/js/2.a5763593.chunk.js"
+  },
+  {
+    "revision": "b5a1abc2693da821edac",
+    "url": "/Project1/static/js/main.8049ca6c.chunk.js"
+  },
+  {
+    "revision": "8a584e3ba5d1570baf65",
+    "url": "/Project1/static/js/runtime~main.05b8ffbb.js"
+  }
+]);
